e6671c91e4ed7e493d2e30153f6d74ed905dab6e
Pranab Chaudhuri <pranab.chaudhuri@microsoft.com>
2020-09-08 17:40:56 +0530

    SnackBar Dual Screen support
    Supporting dual scree in SnackBar

--- FluentUI/src/main/java/com/microsoft/fluentui/snackbar/Snackbar.kt
+++ FluentUI/src/main/java/com/microsoft/fluentui/snackbar/Snackbar.kt
@@ -19,7 +19,9 @@ import android.widget.TextView
 import com.microsoft.fluentui.R
 import com.microsoft.fluentui.R.id.*
 import com.microsoft.fluentui.theming.FluentUIContextThemeWrapper
+import com.microsoft.fluentui.util.DuoSupportUtils
 import com.microsoft.fluentui.util.ThemeUtil
+import com.microsoft.fluentui.util.activity
 import kotlinx.android.synthetic.main.view_snackbar.view.*
 
 /**
@@ -110,6 +112,8 @@ class Snackbar : BaseTransientBottomBar<Snackbar> {
 
     private var customViewSize: CustomViewSize = CustomViewSize.SMALL
     private var style: Style = Style.REGULAR
+    private var singleScreenDisplayPixels = 0
+    private var screenPos = IntArray(2)
 
     private val customViewVerticalMargin: Int
         get() {
@@ -122,19 +126,24 @@ class Snackbar : BaseTransientBottomBar<Snackbar> {
                     CustomViewSize.MEDIUM ->
                         R.dimen.fluentui_snackbar_custom_view_margin_vertical_medium
                 }
-
             return context.resources.getDimension(marginResourceId).toInt()
         }
 
     private constructor(parent: ViewGroup, content: View, contentViewCallback: ContentViewCallback) : super(parent, content, contentViewCallback) {
         updateBackground()
+        singleScreenDisplayPixels = DuoSupportUtils.getSingleScreenWidthPixels(context)
 
         // Set the margin on the FrameLayout (SnackbarLayout) instead of the content because the content's bottom margin is buggy in some APIs.
         if (content.parent is FrameLayout) {
             val lp = content.layoutParams as FrameLayout.LayoutParams
             lp.bottomMargin = context.resources.getDimension(R.dimen.fluentui_snackbar_background_inset).toInt()
+            if(DuoSupportUtils.isWindowDoublePortrait(context)) {
+                lp.width = singleScreenDisplayPixels
+                lp.leftMargin = singleScreenDisplayPixels + DuoSupportUtils.DUO_HINGE_WIDTH
+            }
             content.layoutParams = lp
         }
+
     }
 
     /**
