44d9f22eb4b380852affcf85992fbdf482beec3c
Yat3s <hawkoyates@gmail.com>
2021-01-28 00:09:52 +0800

    Address feedback

--- FluentUI/src/main/java/com/microsoft/fluentui/contextualcommandbar/CommandItem.kt
+++ FluentUI/src/main/java/com/microsoft/fluentui/contextualcommandbar/CommandItem.kt
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 package com.microsoft.fluentui.contextualcommandbar
 
 import android.support.annotation.DrawableRes
