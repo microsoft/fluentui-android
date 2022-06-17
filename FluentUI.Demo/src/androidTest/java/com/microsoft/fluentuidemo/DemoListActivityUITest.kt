616d0dece3e0686a776b0a75107002541b67d3c8
PraveenKumar Yeruva <pyeruva@microsoft.com>
2022-06-17 18:31:03 +0530

    actionBarLayout UI Test

--- FluentUI.Demo/src/androidTest/java/com/microsoft/fluentuidemo/DemoListActivityUITest.kt
+++ FluentUI.Demo/src/androidTest/java/com/microsoft/fluentuidemo/DemoListActivityUITest.kt
@@ -1,24 +1,20 @@
 package com.microsoft.fluentuidemo
 
-import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.Espresso.pressBack
-import androidx.test.espresso.UiController
-import androidx.test.espresso.ViewAction
 import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.Intents.intended
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
 import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.espresso.matcher.ViewMatchers.isRoot
 import androidx.test.ext.junit.rules.ActivityScenarioRule
-import com.microsoft.fluentuidemo.demos.ActionBarLayoutActivity
-import org.hamcrest.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.*
+import org.junit.runner.*
 
+@RunWith(AndroidJUnit4::class)
 class DemoListActivityUITest {
 
     var activities = DEMOS
@@ -34,23 +30,11 @@ class DemoListActivityUITest {
     @Test
     fun testDemoMainActivity(){
         for((position, activity) in activities.withIndex()){
-//            onView(ViewMatchers.withId(R.id.demo_list)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(position))
-//            onView(isRoot()).perform(waitFor(500))
             onView(ViewMatchers.withId(R.id.demo_list)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(position, ViewActions.click()))
             intended(hasComponent(activity.demoClass.qualifiedName))
             pressBack()
             if(activity.title == PEOPLE_PICKER_VIEW){
                 pressBack()
-            }
-        }
-    }
-
-    fun waitFor(delay: Long): ViewAction? {
-        return object : ViewAction {
-            override fun getConstraints(): Matcher<View> = isRoot()
-            override fun getDescription(): String = "wait for $delay milliseconds"
-            override fun perform(uiController: UiController, v: View?) {
-                uiController.loopMainThreadForAtLeast(delay)
             }
         }
     }
