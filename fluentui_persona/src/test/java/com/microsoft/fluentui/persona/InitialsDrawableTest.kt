32ac1490cc44ae7cdc9ac1ee192fe0c641f04d1a
PraveenKumar Yeruva <pyeruva@microsoft.com>
2022-06-17 18:31:03 +0530

    actionBarLayout UI Test

--- fluentui_persona/src/test/java/com/microsoft/fluentui/persona/InitialsDrawableTest.kt
+++ fluentui_persona/src/test/java/com/microsoft/fluentui/persona/InitialsDrawableTest.kt
@@ -8,7 +8,7 @@ class InitialsDrawableTest {
     private val NAME = "Lorem Ipsum"
     private val ONE_WORD_NAME = "Lorem"
     private val EMPTY_NAME = ""
-    private val NAME_WIH_DIGITS = "Lorem 2 Ipsum"
+    private val NAME_WITH_DIGITS = "Lorem 2 Ipsum"
     private val NAME_WITH_SPECIAL_CHAR = "_Lorem @ Ipsum"
     private val EMAIL = "loremipsum@mymail.com"
     private val EMPTY_EMAIL = ""
@@ -30,7 +30,7 @@ class InitialsDrawableTest {
     @Test
     fun testGetInitialsWithDigits(){
         val expectedInitials = "L2"
-        val resultantInitials = InitialsDrawable.getInitials(NAME_WIH_DIGITS, EMAIL)
+        val resultantInitials = InitialsDrawable.getInitials(NAME_WITH_DIGITS, EMAIL)
         Assert.assertEquals(expectedInitials, resultantInitials)
     }
 
