88f101a836aac90330113c8b10153dfb494dabd0
PraveenKumar Yeruva <pyeruva@microsoft.com>
2022-07-04 17:50:57 +0530

    DemolistActivity

--- FluentUI.Demo/src/test/java/com/microsoft/fluentuidemo/DemoListActivityTest.kt
+++ FluentUI.Demo/src/test/java/com/microsoft/fluentuidemo/DemoListActivityTest.kt
@@ -1,4 +1,36 @@
 package com.microsoft.fluentuidemo
 
+import android.app.Activity
+import android.content.Intent
+import androidx.recyclerview.widget.RecyclerView
+import org.junit.*
+import org.junit.runner.*
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.Shadows.shadowOf
+import org.robolectric.shadows.ShadowActivity
+import org.robolectric.shadows.ShadowIntent
+
+@RunWith(RobolectricTestRunner::class)
 class DemoListActivityTest {
+
+    private var activity: Activity? = null
+    private var shadowActivity: ShadowActivity? = null
+
+    @Before
+    fun setup() {
+        activity = Robolectric.buildActivity(DemoListActivity::class.java).create().get()
+        shadowActivity = shadowOf(activity)
+    }
+
+    @Test
+    fun testDemoList(){
+        val recycler = activity?.findViewById<RecyclerView>(R.id.demo_list)
+        for((position,demo) in DEMOS.withIndex()){
+            recycler?.findViewHolderForAdapterPosition(position)?.itemView?.performClick()
+            val startedIntent: Intent? = shadowActivity?.nextStartedActivity
+            val shadowIntent: ShadowIntent = shadowOf(startedIntent)
+            Assert.assertEquals(shadowIntent.intentClass.name, demo.demoClass.java.name)
+        }
+    }
 }
--- FluentUI.Demo/src/test/java/com/microsoft/fluentuidemo/util/AvatarUtilsTest.kt
+++ FluentUI.Demo/src/test/java/com/microsoft/fluentuidemo/util/AvatarUtilsTest.kt
@@ -1,46 +1,46 @@
 package com.microsoft.fluentuidemo.util
-//
-//import android.graphics.BitmapFactory
-//import android.net.Uri
-//import androidx.core.content.ContextCompat
-//import com.microsoft.fluentui.persona.IAvatar
-//import com.microsoft.fluentuidemo.DemoListActivity
-//import com.microsoft.fluentuidemo.R
-//import org.junit.*
-//import org.junit.runner.*
-//import org.robolectric.Robolectric
-//import org.robolectric.RobolectricTestRunner
-//import org.robolectric.RuntimeEnvironment
-//
-//@RunWith(RobolectricTestRunner::class)
-//class AvatarUtilsTest {
-//
-//    private val context = RuntimeEnvironment.application.applicationContext
-//    companion object{
-//        private const val NAME = "Lorem"
-//        private const val EMAIL = "Lorem@mymail.com"
-//        private const val IMAGE_RESOURCE_ID = 121
-//        private val IMAGE_URI = Uri.parse("uri:lorem")
-//        private const val BACKGROUND_COLOR = 10
-//        private const val CONTENT_DESCRIPTION_LABEL = "Lorem Ipsum is great"
-//    }
-//    private val IMAGE_DRAWABLE = ContextCompat.getDrawable(context, R.drawable.avatar_elvia_atkins)
-//    private val IMAGE_BITMAP = BitmapFactory.decodeResource(context.resources, R.drawable.avatar_katri_ahokas)
-//
-//    @Test
-//    fun testCreateAvatar(){
-//        var avatar = generateAvatar()
-//
-//        Assert.assertEquals(NAME, avatar.name)
-//        Assert.assertEquals(IMAGE_RESOURCE_ID, avatar.avatarImageResourceId)
-//        Assert.assertEquals(IMAGE_DRAWABLE, avatar.avatarImageDrawable)
-//        Assert.assertEquals(IMAGE_BITMAP, avatar.avatarImageBitmap)
-//        Assert.assertEquals(IMAGE_URI, avatar.avatarImageUri)
-//        Assert.assertEquals(BACKGROUND_COLOR, avatar.avatarBackgroundColor)
-//        Assert.assertEquals(EMAIL, avatar.email)
-//    }
-//
-//    private fun generateAvatar(): IAvatar{
-//       return createAvatar(NAME, IMAGE_RESOURCE_ID, IMAGE_DRAWABLE, IMAGE_BITMAP, IMAGE_URI, BACKGROUND_COLOR, EMAIL)
-//    }
-//}
+
+import android.graphics.BitmapFactory
+import android.net.Uri
+import androidx.core.content.ContextCompat
+import com.microsoft.fluentui.persona.IAvatar
+import com.microsoft.fluentuidemo.DemoListActivity
+import com.microsoft.fluentuidemo.R
+import org.junit.*
+import org.junit.runner.*
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
+
+@RunWith(RobolectricTestRunner::class)
+class AvatarUtilsTest {
+
+    private val context = RuntimeEnvironment.application.applicationContext
+    companion object{
+        private const val NAME = "Lorem"
+        private const val EMAIL = "Lorem@mymail.com"
+        private const val IMAGE_RESOURCE_ID = 121
+        private val IMAGE_URI = Uri.parse("uri:lorem")
+        private const val BACKGROUND_COLOR = 10
+        private const val CONTENT_DESCRIPTION_LABEL = "Lorem Ipsum is great"
+    }
+    private val IMAGE_DRAWABLE = ContextCompat.getDrawable(context, R.drawable.avatar_elvia_atkins)
+    private val IMAGE_BITMAP = BitmapFactory.decodeResource(context.resources, R.drawable.avatar_katri_ahokas)
+
+    @Test
+    fun testCreateAvatar(){
+        var avatar = generateAvatar()
+
+        Assert.assertEquals(NAME, avatar.name)
+        Assert.assertEquals(IMAGE_RESOURCE_ID, avatar.avatarImageResourceId)
+        Assert.assertEquals(IMAGE_DRAWABLE, avatar.avatarImageDrawable)
+        Assert.assertEquals(IMAGE_BITMAP, avatar.avatarImageBitmap)
+        Assert.assertEquals(IMAGE_URI, avatar.avatarImageUri)
+        Assert.assertEquals(BACKGROUND_COLOR, avatar.avatarBackgroundColor)
+        Assert.assertEquals(EMAIL, avatar.email)
+    }
+
+    private fun generateAvatar(): IAvatar{
+       return createAvatar(NAME, IMAGE_RESOURCE_ID, IMAGE_DRAWABLE, IMAGE_BITMAP, IMAGE_URI, BACKGROUND_COLOR, EMAIL)
+    }
+}
