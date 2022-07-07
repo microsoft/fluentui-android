9f039cdf6a0de97fdb3c878e2aa1b30e5d470f4f
PraveenKumar Yeruva <pyeruva@microsoft.com>
2022-07-07 16:32:06 +0530

    More tests

--- FluentUI.Demo/src/androidTest/java/com/microsoft/fluentuidemo/demos/AppBarLayoutActivityUITest.kt
+++ FluentUI.Demo/src/androidTest/java/com/microsoft/fluentuidemo/demos/AppBarLayoutActivityUITest.kt
@@ -1,20 +1,26 @@
 package com.microsoft.fluentuidemo.demos
 
 import android.content.Intent
-import androidx.recyclerview.widget.RecyclerView
+import androidx.appcompat.widget.AppCompatImageButton
+import androidx.appcompat.widget.AppCompatTextView
 import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onData
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.assertion.PositionAssertions
 import androidx.test.espresso.assertion.ViewAssertions
-import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.intent.Intents
-import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.matcher.ViewMatchers.Visibility.VISIBLE
 import androidx.test.platform.app.InstrumentationRegistry
+import com.microsoft.fluentui.appbarlayout.AppBarLayout
+import com.microsoft.fluentui.toolbar.Toolbar
 import com.microsoft.fluentuidemo.DemoActivity
 import com.microsoft.fluentuidemo.R.id
+import org.hamcrest.Matchers.*
 import org.junit.*
 import java.util.*
+import com.microsoft.fluentui.search.Searchbar
+import com.microsoft.fluentuidemo.demos.list.ListSubHeader
 
 class AppBarLayoutActivityUITest {
 
@@ -31,37 +37,74 @@ class AppBarLayoutActivityUITest {
     @Before
     fun initialize(){
         Intents.init()
+        launchActivity()
     }
 
     @Test
     fun testScrollBehaviorCollapseToolbar(){
-        launchActivity()
+        onData(startsWith("Scroll behavior")).check(ViewAssertions.matches(withText("Scroll behavior: COLLAPSE_TOOLBAR")))
+        onView(allOf(isAssignableFrom(Searchbar::class.java), isDescendantOfA(isAssignableFrom(AppBarLayout::class.java)))).perform(ViewActions.click())
 
-        onView(ViewMatchers.withId(id.app_bar_layout_toggle_scroll_behavior_button)).perform(ViewActions.click(), ViewActions.click(), ViewActions.click())
-        onView(ViewMatchers.withId(id.app_bar_layout_list)).perform(ViewActions.scrollTo())
-
-        onView(ViewMatchers.withId(id.app_bar_layout_action_search)).check(PositionAssertions.isTopAlignedWith(
-            ViewMatchers.withId(id.parent)))
+        onView(isAssignableFrom(AppBarLayout::class.java)).check(ViewAssertions.matches(
+            not(isCompletelyDisplayed())
+        ))
     }
 
     @Test
     fun testScrollBehaviorPin(){
-        launchActivity()
+        onView(withId(id.app_bar_layout_toggle_scroll_behavior_button)).perform(ViewActions.click())
+        onView(withId(id.app_bar_layout_list)).perform(ViewActions.swipeUp())
 
-        onView(ViewMatchers.withId(id.app_bar_layout_toggle_scroll_behavior_button)).perform(ViewActions.click())
-        onView(ViewMatchers.withId(id.app_bar_layout_list)).perform(ViewActions.swipeUp())
+        onView(withId(id.app_bar)).check(ViewAssertions.matches(isCompletelyDisplayed()))
 
-        onView(ViewMatchers.withId(id.app_bar)).check(ViewAssertions.matches(ViewMatchers.withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))
+        onView(withId(id.app_bar_layout_list)).perform(ViewActions.swipeDown())
+        onView(allOf(isAssignableFrom(Searchbar::class.java), isDescendantOfA(isAssignableFrom(AppBarLayout::class.java)))).perform(ViewActions.click())
+
+        onView(withId(id.app_bar)).check(ViewAssertions.matches(isCompletelyDisplayed()))
     }
 
     @Test
     fun testScrollBehaviorNone(){
-        launchActivity()
+        onView(withId(id.app_bar_layout_toggle_scroll_behavior_button)).perform(ViewActions.click(), ViewActions.click())
+        onView(withId(id.app_bar_layout_list)).perform(ViewActions.swipeUp())
 
-        onView(ViewMatchers.withId(id.app_bar_layout_toggle_scroll_behavior_button)).perform(ViewActions.click(), ViewActions.click())
-        onView(ViewMatchers.withId(id.app_bar_layout_list)).perform(ViewActions.swipeUp())
+        onView(withId(id.app_bar)).check(ViewAssertions.matches(isCompletelyDisplayed()))
 
-        onView(ViewMatchers.withId(id.app_bar)).check(ViewAssertions.matches(ViewMatchers.withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))
+        onView(withId(id.app_bar_layout_list)).perform(ViewActions.swipeDown())
+        onView(allOf(isAssignableFrom(Searchbar::class.java), isDescendantOfA(isAssignableFrom(AppBarLayout::class.java)))).perform(ViewActions.click())
+
+        onView(withId(id.app_bar)).check(ViewAssertions.matches(isCompletelyDisplayed()))
+    }
+
+    @Test
+    fun testToggleNavigationIcon(){
+        //back arrow
+        onView(allOf(isAssignableFrom(AppCompatImageButton::class.java), isDescendantOfA(isAssignableFrom(Toolbar::class.java)))).check(ViewAssertions.matches(
+            withEffectiveVisibility(VISIBLE)))
+
+        onView(withId(id.app_bar_layout_toggle_navigation_icon_button)).perform(ViewActions.click())
+        //No icon
+        onView(isAssignableFrom(Toolbar::class.java)).check(ViewAssertions.matches(
+            not(withChild(isAssignableFrom(AppCompatImageButton::class.java)))))
+
+        onView(withId(id.app_bar_layout_toggle_navigation_icon_button)).perform(ViewActions.click())
+        //Avatar
+        onView(allOf(isAssignableFrom(AppCompatImageButton::class.java), isDescendantOfA(isAssignableFrom(Toolbar::class.java)))).check(ViewAssertions.matches(
+            withEffectiveVisibility(VISIBLE)))
+    }
+
+    @Test
+    fun testToggleSearchbarLayoutStyle(){
+        onView(allOf(isAssignableFrom(Searchbar::class.java), isDescendantOfA(isAssignableFrom(AppBarLayout::class.java)))).check(ViewAssertions.matches(withEffectiveVisibility(VISIBLE)))
+
+        onView(withId(id.app_bar_layout_toggle_searchbar_type_button)).perform(ViewActions.click())
+
+        onView(isAssignableFrom(AppBarLayout::class.java)).check(ViewAssertions.matches(not(withChild(isAssignableFrom(Searchbar::class.java)))))
+    }
+
+    @Test
+    fun testToggleTheme(){
+
     }
 
     @After
