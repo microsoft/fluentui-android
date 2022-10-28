8ae1a05e312783461c2d96b9aca56f34980d72c1
PraveenKumar yeruva <praveenyeruva970@gmail.com>
2022-10-28 17:16:15 +0530

    Updating hydralab plugin (#271)
    * Renaming persona activities and component. Updating hydra plugin and parameters
    
    * reformat file
    
    Co-authored-by: PraveenKumar Yeruva <pyeruva@microsoft.com>

--- FluentUI.Demo/src/main/java/com/microsoft/fluentuidemo/demos/V2PersonaActivity.kt
+++ FluentUI.Demo/src/main/java/com/microsoft/fluentuidemo/demos/V2PersonaActivity.kt
@@ -2,11 +2,8 @@ package com.microsoft.fluentuidemo.demos
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.widget.Toast
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
@@ -19,7 +16,7 @@ import androidx.compose.ui.unit.dp
 import com.microsoft.fluentui.theme.FluentTheme
 import com.microsoft.fluentui.theme.token.controlTokens.AvatarStatus
 import com.microsoft.fluentui.tokenized.persona.Person
-import com.microsoft.fluentui.tokenized.persona.PersonaView
+import com.microsoft.fluentui.tokenized.persona.Persona
 import com.microsoft.fluentuidemo.DemoActivity
 import com.microsoft.fluentuidemo.R
 import kotlinx.coroutines.CoroutineScope
@@ -42,40 +39,93 @@ class V2PersonaActivity : DemoActivity() {
             }
         }
     }
+
     @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
     @Composable
-    fun createActivityUI(){
-        val person1 = Person("Allan", "Munger",
+    fun createActivityUI() {
+        val person1 = Person(
+            "Allan", "Munger",
             image = R.drawable.avatar_allan_munger, isActive = true,
-            status = AvatarStatus.Available, isOOO = false)
-        val person2 = Person("Charlotte", "Waltson",
+            status = AvatarStatus.Available, isOOO = false
+        )
+        val person2 = Person(
+            "Charlotte", "Waltson",
             image = R.drawable.avatar_charlotte_waltson, isActive = false,
-            status = AvatarStatus.Blocked, isOOO = false)
-        val person3 = Person("Carole", "Poland",
+            status = AvatarStatus.Blocked, isOOO = false
+        )
+        val person3 = Person(
+            "Carole", "Poland",
             image = R.drawable.avatar_carole_poland, isActive = false,
-            status = AvatarStatus.Away, isOOO = true)
+            status = AvatarStatus.Away, isOOO = true
+        )
         val scaffoldState: ScaffoldState = rememberScaffoldState()
         val coroutineScope: CoroutineScope = rememberCoroutineScope()
-        Scaffold(scaffoldState = scaffoldState ) {
-            Box(){
-                Column() {
-                    LazyColumn(){
+        Scaffold(scaffoldState = scaffoldState) {
+            Box {
+                Column {
+                    LazyColumn {
                         item {
-                            Column() {
-                                Text(modifier = Modifier.padding(start = 8.dp, top = 16.dp), text = "One line Persona view with small Avatar", color = Color(0xFF2886DE))
-                                PersonaView(person = person1, primaryText = person1.firstName+" "+person1.lastName, onClick = { onClick(person1.firstName, coroutineScope, scaffoldState) }, enableAvatarActivityRings = true)
+                            Column {
+                                Text(
+                                    modifier = Modifier.padding(start = 8.dp, top = 16.dp),
+                                    text = "One line Persona view with small Avatar",
+                                    color = Color(0xFF2886DE)
+                                )
+                                Persona(
+                                    person = person1,
+                                    primaryText = person1.firstName + " " + person1.lastName,
+                                    onClick = {
+                                        onClick(
+                                            person1.firstName,
+                                            coroutineScope,
+                                            scaffoldState
+                                        )
+                                    },
+                                    enableAvatarActivityRings = true
+                                )
                             }
                         }
                         item {
-                            Column() {
-                                Text(modifier = Modifier.padding(start = 8.dp, top = 16.dp), text = "Two line Persona view with large Avatar", color = Color(0xFF2886DE))
-                                PersonaView(person = person2, primaryText = person2.firstName+" "+person2.lastName, secondaryText = "Microsoft", onClick = { onClick(person2.firstName, coroutineScope, scaffoldState) }, )
+                            Column {
+                                Text(
+                                    modifier = Modifier.padding(start = 8.dp, top = 16.dp),
+                                    text = "Two line Persona view with large Avatar",
+                                    color = Color(0xFF2886DE)
+                                )
+                                Persona(
+                                    person = person2,
+                                    primaryText = person2.firstName + " " + person2.lastName,
+                                    secondaryText = "Microsoft",
+                                    onClick = {
+                                        onClick(
+                                            person2.firstName,
+                                            coroutineScope,
+                                            scaffoldState
+                                        )
+                                    },
+                                )
                             }
                         }
                         item {
-                            Column() {
-                                Text(modifier = Modifier.padding(start = 8.dp, top = 16.dp), text = "Three line Persona View with Xlarge Avatar", color = Color(0xFF2886DE))
-                                PersonaView(person = person3, primaryText = person3.firstName+" "+person3.lastName, secondaryText = "Microsoft", tertiaryText = person3.status.toString(), onClick = { onClick(person3.firstName, coroutineScope, scaffoldState) }, )
+                            Column {
+                                Text(
+                                    modifier = Modifier.padding(start = 8.dp, top = 16.dp),
+                                    text = "Three line Persona View with Xlarge Avatar",
+                                    color = Color(0xFF2886DE)
+                                )
+                                Persona(
+                                    person = person3,
+                                    primaryText = person3.firstName + " " + person3.lastName,
+                                    secondaryText = "Microsoft",
+                                    tertiaryText = person3.status.toString(),
+                                    onClick = {
+                                        onClick(
+                                            person3.firstName,
+                                            coroutineScope,
+                                            scaffoldState
+                                        )
+                                    },
+                                )
                             }
                         }
                     }
@@ -85,14 +135,15 @@ class V2PersonaActivity : DemoActivity() {
         }
 
     }
-    fun onClick(text: String, coroutineScope:CoroutineScope, scaffoldState: ScaffoldState){
+
+    fun onClick(text: String, coroutineScope: CoroutineScope, scaffoldState: ScaffoldState) {
         scaffoldState.snackbarHostState.currentSnackbarData?.dismiss()
         coroutineScope.launch {
             val result = scaffoldState.snackbarHostState.showSnackbar(
                 message = "Clicked on $text",
                 actionLabel = "Close"
             )
-            when(result){
+            when (result) {
                 SnackbarResult.ActionPerformed -> scaffoldState.snackbarHostState.currentSnackbarData?.dismiss()
             }
         }
