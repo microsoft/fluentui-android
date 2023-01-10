1bc1c6d075ce33882f50826d19e3ef034c54612a
PraveenKumar yeruva <praveenyeruva970@gmail.com>
2023-01-10 13:52:30 +0530

    adding rtl support in linear progress and shimmer (#313)
    * adding rtl support in linear progress and shimmer
    
    * refactor bar to indicator
    
    Co-authored-by: PraveenKumar Yeruva <pyeruva@microsoft.com>

--- fluentui_progress/src/main/java/com/microsoft/fluentui/tokenized/progress/LinearProgressIndicator.kt
+++ fluentui_progress/src/main/java/com/microsoft/fluentui/tokenized/progress/LinearProgressIndicator.kt
@@ -1,5 +1,6 @@
 package com.microsoft.fluentui.tokenized.progress
 
+import android.widget.ProgressBar
 import androidx.compose.animation.core.*
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,6 +12,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.unit.LayoutDirection
 import com.microsoft.fluentui.theme.FluentTheme
 import com.microsoft.fluentui.theme.token.ControlTokens
 import com.microsoft.fluentui.theme.token.controlTokens.LinearProgressIndicatorHeight
@@ -82,16 +84,21 @@ fun LinearProgressIndicator(
         ) {
             val strokeWidth = dpToPx(linearProgressIndicatorHeight)
             val yOffset = strokeWidth / 2
+            val isLtr = layoutDirection == LayoutDirection.Ltr
+            val barStart = (if (isLtr) 0f else size.width)
+            val barEnd = (if (isLtr) size.width else 0f)
             drawLine(
                 linearProgressIndicatorBackgroundColor,
-                Offset(0f, yOffset),
-                Offset(size.width, yOffset),
+                Offset(barStart, yOffset),
+                Offset(barEnd, yOffset),
                 strokeWidth
             )
+            val progressIndicatorWidth = currentProgress.value * size.width
+            val indicatorLineEnd = if(isLtr) progressIndicatorWidth else size.width - progressIndicatorWidth
             drawLine(
                 linearProgressIndicatorColor,
-                Offset(0f, yOffset),
-                Offset(currentProgress.value * (size.width), yOffset),
+                Offset(barStart, yOffset),
+                Offset(indicatorLineEnd, yOffset),
                 strokeWidth
             )
         }
@@ -165,22 +172,27 @@ fun LinearProgressIndicator(
         ) {
             val strokeWidth = dpToPx(linearProgressIndicatorHeight)
             val yOffset = strokeWidth / 2
+            val isLtr = layoutDirection == LayoutDirection.Ltr
+            val barStart = (if (isLtr) 0f else size.width)
+            val barEnd = (if (isLtr) size.width else 0f)
             drawLine(
                 linearProgressIndicatorBackgroundColor,
-                Offset(0f, yOffset),
-                Offset(size.width, yOffset),
+                Offset(barStart, yOffset),
+                Offset(barEnd, yOffset),
                 strokeWidth
             )
+            val progressIndicatorStart = (if (isLtr) indicatorHead * size.width else (size.width) - indicatorHead * size.width)
+            val progressIndicatorEnd = (if (isLtr) indicatorTail * (size.width) else (size.width) - indicatorTail * (size.width))
             drawLine(
                 Brush.linearGradient(
                     0f to linearProgressIndicatorBackgroundColor,
                     0.5f to linearProgressIndicatorColor,
                     1.0f to linearProgressIndicatorBackgroundColor,
-                    start = Offset(indicatorHead * size.width, yOffset),
-                    end = Offset(indicatorTail * size.width, yOffset)
+                    start = Offset(progressIndicatorStart, yOffset),
+                    end = Offset(progressIndicatorEnd, yOffset)
                 ),
-                Offset(indicatorHead * size.width, yOffset),
-                Offset(indicatorTail * size.width, yOffset),
+                Offset(progressIndicatorStart, yOffset),
+                Offset(progressIndicatorEnd, yOffset),
                 strokeWidth
             )
         }
