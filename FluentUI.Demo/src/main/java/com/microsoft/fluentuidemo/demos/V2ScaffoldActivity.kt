0f6b590745cef72f3860fcd7c2d514b1a8546584
NamanPandey <namanpandey@microsoft.com>
2023-04-04 13:03:12 +0530

    Add Snackbar to Scaffold Activity and Test (#388)

--- FluentUI.Demo/src/main/java/com/microsoft/fluentuidemo/demos/V2ScaffoldActivity.kt
+++ FluentUI.Demo/src/main/java/com/microsoft/fluentuidemo/demos/V2ScaffoldActivity.kt
@@ -13,14 +13,17 @@ import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.outlined.Email
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material.icons.outlined.ShoppingCart
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
 import com.microsoft.fluentui.compose.Scaffold
 import com.microsoft.fluentui.theme.FluentTheme
+import com.microsoft.fluentui.theme.token.FluentIcon
 import com.microsoft.fluentui.theme.token.controlTokens.*
 import com.microsoft.fluentui.tokenized.AppBar
 import com.microsoft.fluentui.tokenized.controls.Button
@@ -31,6 +34,9 @@ import com.microsoft.fluentui.tokenized.listitem.ListItem
 import com.microsoft.fluentui.tokenized.navigation.TabBar
 import com.microsoft.fluentui.tokenized.navigation.TabData
 import com.microsoft.fluentui.tokenized.notification.Badge
+import com.microsoft.fluentui.tokenized.notification.NotificationResult
+import com.microsoft.fluentui.tokenized.notification.Snackbar
+import com.microsoft.fluentui.tokenized.notification.SnackbarState
 import com.microsoft.fluentuidemo.DemoActivity
 import com.microsoft.fluentuidemo.R
 import com.microsoft.fluentuidemo.icons.ListItemIcons
@@ -48,8 +54,11 @@ class V2ScaffoldActivity : DemoActivity() {
     private val TOP_BAR = "TopBar"
     private val BOTTOM_BAR = "BottomBar"
     private val SNACKBAR = "SnackBar"
+    private val DRAWER = "Drawer"
     private val FLOATING_ACTION_BUTTON = "FAB"
     private val MAIN_CONTENT = "Main Content"
+    private val DRAWER_BUTTON = "Drawer Button"
+    private val SNACKBAR_BUTTON = "Snackbar Button"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -93,6 +102,7 @@ class V2ScaffoldActivity : DemoActivity() {
                 )
             )
             var fabState by rememberSaveable { mutableStateOf(FABState.Expanded) }
+            val snackbarState by remember { mutableStateOf(SnackbarState()) }
 
             FluentTheme {
                 Scaffold(
@@ -131,65 +141,123 @@ class V2ScaffoldActivity : DemoActivity() {
                             },
                             modifier = Modifier.testTag(FLOATING_ACTION_BUTTON)
                         )
+                    },
+                    snackbar = {
+                        Snackbar(
+                            snackbarState = snackbarState,
+                            modifier = Modifier.testTag(SNACKBAR)
+                        )
                     }
                 ) {
                     Box(
                         Modifier
                             .padding(it)
-                            .testTag(MAIN_CONTENT)) {
-                        GetContent(context)
+                            .testTag(MAIN_CONTENT)
+                    ) {
+                        GetContent(context, snackbarState)
                     }
                 }
             }
         }
     }
-}
 
-@Composable
-private fun GetContent(context: Context) {
-    val size = remember { mutableStateOf(5) }
-    val drawerState = rememberDrawerState()
-    val scope = rememberCoroutineScope()
-    Column {
-        Drawer(
-            drawerState = drawerState,
-            drawerContent = { CreateList(size = 20, context = context) }
-        )
-        Row(modifier = Modifier.padding(8.dp), horizontalArrangement = Arrangement.spacedBy(8.dp)) {
-            Button(
-                style = ButtonStyle.OutlinedButton,
-                size = ButtonSize.Medium,
-                text = context.resources.getString(R.string.scaffold_refresh_list),
-                onClick = { size.value = (40 * Math.random()).toInt() })
+    @Composable
+    private fun GetContent(context: Context, snackbarState: SnackbarState? = null) {
+        val size = remember { mutableStateOf(5) }
+        val drawerState = rememberDrawerState()
+        val scope = rememberCoroutineScope()
+        Column {
+            Drawer(
+                modifier = Modifier.testTag(DRAWER),
+                drawerState = drawerState,
+                drawerContent = { CreateList(size = 20, context = context) }
+            )
+            Row(
+                modifier = Modifier.padding(8.dp),
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                Button(
+                    style = ButtonStyle.OutlinedButton,
+                    size = ButtonSize.Medium,
+                    text = context.resources.getString(R.string.scaffold_refresh_list),
+                    onClick = { size.value = (40 * Math.random()).toInt() })
 
-            Button(
-                style = ButtonStyle.OutlinedButton,
-                size = ButtonSize.Medium,
-                text = context.resources.getString(R.string.scaffold_open_drawer),
-                onClick = { scope.launch { drawerState.open() } })
+                Button(
+                    modifier = Modifier.testTag(DRAWER_BUTTON),
+                    style = ButtonStyle.OutlinedButton,
+                    size = ButtonSize.Medium,
+                    text = context.resources.getString(R.string.scaffold_open_drawer),
+                    onClick = { scope.launch { drawerState.open() } })
+
+                if (snackbarState != null) {
+                    val snackBarTitle =
+                        LocalContext.current.resources.getString(R.string.fluentui_title)
+                    val actionButtonString =
+                        LocalContext.current.resources.getString(R.string.fluentui_action_button)
+                    val dismissedString =
+                        LocalContext.current.resources.getString(R.string.fluentui_dismissed)
+                    val pressedString =
+                        LocalContext.current.resources.getString(R.string.fluentui_button_pressed)
+                    val timeoutString =
+                        LocalContext.current.resources.getString(R.string.fluentui_timeout)
+                    var displayString: String = ""
+                    Button(
+                        modifier = Modifier.testTag(SNACKBAR_BUTTON),
+                        style = ButtonStyle.OutlinedButton,
+                        size = ButtonSize.Medium,
+                        text = context.resources.getString(R.string.fluentui_show_snackbar),
+                        onClick = {
+                            scope.launch {
+                                val result: NotificationResult = snackbarState.showSnackbar(
+                                    message = snackBarTitle,
+                                    style = SnackbarStyle.Contrast,
+                                    icon = FluentIcon(Icons.Outlined.ShoppingCart),
+                                    actionText = actionButtonString
+                                )
+
+                                when (result) {
+                                    NotificationResult.TIMEOUT ->
+                                        displayString = timeoutString
+
+                                    NotificationResult.CLICKED ->
+                                        displayString = pressedString
+
+                                    NotificationResult.DISMISSED ->
+                                        displayString = dismissedString
+                                }
+                                invokeToast(displayString, context)
+                            }
+                        }
+                    )
+                }
+            }
+            CreateList(size.value, context)
         }
-        CreateList(size.value, context)
     }
-}
 
-@Composable
-fun CreateList(size: Int, context: Context) {
-    LazyColumn()
-    {
-        repeat(size) {
-            item {
-                ListItem.Item(text = context.resources.getString(R.string.common_list, it),
-                    onClick = {
-                        invokeToast(context.resources.getString(R.string.common_list, it), context)
-                    },
-                    leadingAccessoryView = {
-                        Image(
-                            ListItemIcons.Folder40,
-                            context.resources.getString(R.string.common_folder)
-                        )
-                    })
+    @Composable
+    fun CreateList(size: Int, context: Context) {
+        LazyColumn()
+        {
+            repeat(size) {
+                item {
+                    ListItem.Item(text = context.resources.getString(R.string.common_list, it),
+                        onClick = {
+                            invokeToast(
+                                context.resources.getString(R.string.common_list, it),
+                                context
+                            )
+                        },
+                        leadingAccessoryView = {
+                            Image(
+                                ListItemIcons.Folder40,
+                                context.resources.getString(R.string.common_folder)
+                            )
+                        })
+                }
             }
         }
     }
+
 }
 
