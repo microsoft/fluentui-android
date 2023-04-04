43b7c3c3ffe9cf77e8f5bc02138ad1aa50c30e85
NamanPandey <namanpandey.2025@gmail.com>
2023-04-04 12:57:10 +0530

    Add Snackbar to Scaffold Activity and Test

--- FluentUI.Demo/src/androidTest/java/com/microsoft/fluentuidemo/demos/V2ScaffoldActivityUITest.kt
+++ FluentUI.Demo/src/androidTest/java/com/microsoft/fluentuidemo/demos/V2ScaffoldActivityUITest.kt
@@ -2,11 +2,14 @@ package com.microsoft.fluentuidemo.demos
 
 import android.content.Intent
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onAllNodesWithTag
 import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.intent.Intents
 import androidx.test.platform.app.InstrumentationRegistry
 import com.microsoft.fluentuidemo.DemoActivity
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -38,15 +41,40 @@ class V2ScaffoldActivityUITest {
     private val TOP_BAR = "TopBar"
     private val BOTTOM_BAR = "BottomBar"
     private val SNACKBAR = "SnackBar"
+    private val DRAWER = "Drawer"
     private val FLOATING_ACTION_BUTTON = "FAB"
     private val MAIN_CONTENT = "Main Content"
+    private val DRAWER_BUTTON = "Drawer Button"
+    private val SNACKBAR_BUTTON = "Snackbar Button"
 
     @Test
-    fun test(){
+    fun testOnScreenComponents() {
         composeTestRule.onNodeWithTag(TOP_BAR).assertExists("Top Bar not shown")
         composeTestRule.onNodeWithTag(BOTTOM_BAR).assertExists("Bottom Bar not shown")
-//        composeTestRule.onNodeWithTag(SNACKBAR).assertExists("Top Bar not shown")
         composeTestRule.onNodeWithTag(FLOATING_ACTION_BUTTON).assertExists("FAB not shown")
         composeTestRule.onNodeWithTag(MAIN_CONTENT).assertExists("Main content not shown")
+    }
+
+    @Test
+    fun testSnackbar() {
+        composeTestRule.onNodeWithTag(SNACKBAR_BUTTON).performClick()
+        composeTestRule.waitUntil(1000L) {
+            composeTestRule.onAllNodesWithTag(SNACKBAR).fetchSemanticsNodes()
+                .isNotEmpty()
+        }
+    }
+
+    @Test
+    fun testDrawer() {
+        composeTestRule.onNodeWithTag(DRAWER_BUTTON).performClick()
+        composeTestRule.waitUntil(1000L) {
+            composeTestRule.onAllNodesWithTag(DRAWER).fetchSemanticsNodes()
+                .isNotEmpty()
+        }
+    }
+
+    @After
+    fun tearDown() {
+        Intents.release()
     }
 }
