4ed1a314fa6352a15a6238261d15b484dcfb1c04
NamanPandey <namanpandey@microsoft.com>
2023-04-25 16:43:30 +0530

    Implementation for TextField (#396)

--- fluentui_core/src/main/java/com/microsoft/fluentui/theme/token/controlTokens/DividerTokens.kt
+++ fluentui_core/src/main/java/com/microsoft/fluentui/theme/token/controlTokens/DividerTokens.kt
@@ -1,8 +1,11 @@
 package com.microsoft.fluentui.theme.token.controlTokens
 
 import android.os.Parcelable
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.microsoft.fluentui.theme.FluentTheme
 import com.microsoft.fluentui.theme.token.AliasTokens
 import com.microsoft.fluentui.theme.token.ControlInfo
@@ -26,4 +29,12 @@ open class DividerTokens : ControlToken, Parcelable {
             themeMode = FluentTheme.themeMode
         )
     }
+
+    @Composable
+    open fun verticalPadding(dividerInfo: DividerInfo): PaddingValues {
+        return PaddingValues(vertical = 8.dp)
+    }
+
+    @Composable
+    open fun startIndent(dividerInfo: DividerInfo): Dp = 0.dp
 }
