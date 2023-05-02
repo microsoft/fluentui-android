e9636c0c9de7b7b550303596071d517b73368ab0
PraveenKumar <pyeruva@microsoft.com>
2023-05-02 13:42:43 +0530

    removed unused code and minor changes

--- fluentui_core/src/main/java/com/microsoft/fluentui/theme/token/controlTokens/AnnouncementCardTokens.kt
+++ fluentui_core/src/main/java/com/microsoft/fluentui/theme/token/controlTokens/AnnouncementCardTokens.kt
@@ -12,70 +12,80 @@ import com.microsoft.fluentui.theme.token.GlobalTokens
 import com.microsoft.fluentui.theme.token.StateColor
 import kotlinx.parcelize.Parcelize
 
-data class AnnouncementCardInfo(override val cardType: CardType = CardType.Elevated, val buttonType: ButtonStyle = ButtonStyle.TextButton) : BasicCardControlInfo()
+data class AnnouncementCardInfo(
+    override val cardType: CardType = CardType.Elevated,
+    val buttonType: ButtonStyle = ButtonStyle.TextButton
+) : BasicCardControlInfo()
+
 @Parcelize
-open class AnnouncementCardTokens: BasicCardTokens(), Parcelable{
+open class AnnouncementCardTokens : BasicCardTokens(), Parcelable {
     @Composable
     open fun textColor(announcementCardInfo: BasicCardControlInfo): Color {
         return FluentTheme.aliasTokens.neutralForegroundColor[AliasTokens.NeutralForegroundColorTokens.Foreground2].value(
             themeMode = FluentTheme.themeMode
         )
     }
+
     @Composable
     open fun titleColor(announcementCardInfo: BasicCardControlInfo): Color {
         return FluentTheme.aliasTokens.neutralForegroundColor[AliasTokens.NeutralForegroundColorTokens.Foreground1].value(
             themeMode = FluentTheme.themeMode
         )
     }
+
     @Composable
     open fun titleSize(announcementCardInfo: BasicCardControlInfo): TextStyle {
         return FluentTheme.aliasTokens.typography[AliasTokens.TypographyTokens.Body1Strong]
     }
+
     @Composable
     open fun textSize(announcementCardInfo: BasicCardControlInfo): TextStyle {
         return FluentTheme.aliasTokens.typography[AliasTokens.TypographyTokens.Body2]
     }
+
     @Composable
-    open fun buttonColor(announcementCardInfo: BasicCardControlInfo): Color {
-        return FluentTheme.aliasTokens.neutralForegroundColor[AliasTokens.NeutralForegroundColorTokens.Foreground1].value(
-            themeMode = FluentTheme.themeMode
-        )
-    }
-    @Composable
-    open fun previewTitlePadding(announcementCardInfo: BasicCardControlInfo):Dp{
+    open fun previewTitlePadding(announcementCardInfo: BasicCardControlInfo): Dp {
         return GlobalTokens.size(GlobalTokens.SizeTokens.Size160)
     }
+
     @Composable
-    open fun titleTextPadding(announcementCardInfo: BasicCardControlInfo):Dp{
+    open fun titleTextPadding(announcementCardInfo: BasicCardControlInfo): Dp {
         return GlobalTokens.size(GlobalTokens.SizeTokens.Size40)
     }
+
     @Composable
-    open fun textButtonPadding(announcementCardInfo: BasicCardControlInfo):Dp{
+    open fun textButtonPadding(announcementCardInfo: BasicCardControlInfo): Dp {
         return GlobalTokens.size(GlobalTokens.SizeTokens.Size40)
     }
+
     @Composable
-    open fun textHorizontalPadding(announcementCardInfo: BasicCardControlInfo):Dp{
+    open fun textHorizontalPadding(announcementCardInfo: BasicCardControlInfo): Dp {
         return GlobalTokens.size(GlobalTokens.SizeTokens.Size160)
     }
+
     @Composable
-    open fun previewPadding(announcementCardInfo: BasicCardControlInfo):Dp{
+    open fun previewPadding(announcementCardInfo: BasicCardControlInfo): Dp {
         return GlobalTokens.size(GlobalTokens.SizeTokens.Size80)
     }
+
     @Composable
-    open fun previewCornerRadius(announcementCardInfo: BasicCardControlInfo): Dp{
+    open fun previewCornerRadius(announcementCardInfo: BasicCardControlInfo): Dp {
         return GlobalTokens.cornerRadius(GlobalTokens.CornerRadiusTokens.CornerRadius40)
     }
+
     @Composable
     override fun cornerRadius(announcementCardInfo: BasicCardControlInfo): Dp {
         return GlobalTokens.cornerRadius(GlobalTokens.CornerRadiusTokens.CornerRadius80)
     }
+
     @Composable
     override fun elevation(announcementCardInfo: BasicCardControlInfo): Dp {
-        return when(announcementCardInfo.cardType){
+        return when (announcementCardInfo.cardType) {
             CardType.Elevated -> GlobalTokens.elevation(GlobalTokens.ShadowTokens.Shadow64)
             CardType.Outlined -> 0.dp
         }
     }
+
     @Composable
     open fun buttonTextColor(announcementCardInfo: BasicCardControlInfo): StateColor {
         announcementCardInfo as AnnouncementCardInfo
