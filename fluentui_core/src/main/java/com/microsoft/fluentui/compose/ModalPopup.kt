bcff31e5df3bfcf6bd8808454b8c58bcf3bd93c7
mishramayank1 <97503451+mishramayank1@users.noreply.github.com>
2023-08-14 18:06:41 +0530

    Bottom Drawer PopUp Update (#472)
    * update api description
    
    * Fix testcase failure
    
    * Removed unused import

---
+++ fluentui_core/src/main/java/com/microsoft/fluentui/compose/ModalPopup.kt
@@ -0,0 +1,213 @@
+package com.microsoft.fluentui.compose
+
+import android.content.Context
+import android.graphics.PixelFormat
+import android.view.Gravity
+import android.view.KeyEvent
+import android.view.View
+import android.view.ViewTreeObserver
+import android.view.WindowManager
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionContext
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCompositionContext
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.AbstractComposeView
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.platform.ViewRootForInspector
+import androidx.compose.ui.semantics.popup
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.lifecycle.ViewTreeLifecycleOwner
+import androidx.lifecycle.ViewTreeViewModelStoreOwner
+import androidx.savedstate.findViewTreeSavedStateRegistryOwner
+import androidx.savedstate.setViewTreeSavedStateRegistryOwner
+import java.util.UUID
+import kotlin.math.roundToInt
+
+/**
+ * Popup specific for modal bottom drawer.
+ */
+@Composable
+fun ModalPopup(
+    onDismissRequest: () -> Unit,
+    windowInsets: WindowInsets,
+    content: @Composable () -> Unit,
+) {
+    val view = LocalView.current
+    val id = rememberSaveable { UUID.randomUUID() }
+    val parentComposition = rememberCompositionContext()
+    val currentContent by rememberUpdatedState(content)
+    val layoutDirection = LocalLayoutDirection.current
+    val modalWindow = remember {
+        ModalWindow(
+            onDismissRequest = onDismissRequest,
+            composeView = view,
+            saveId = id
+        ).apply {
+            setCustomContent(
+                parent = parentComposition,
+                content = {
+                    Box(
+                        Modifier
+                            .semantics { this.popup() }
+                            .windowInsetsPadding(windowInsets)
+                            .imePadding()
+                    ) {
+                        currentContent()
+                    }
+                }
+            )
+        }
+    }
+
+    DisposableEffect(modalWindow) {
+        modalWindow.show()
+        modalWindow.superSetLayoutDirection(layoutDirection)
+        onDispose {
+            modalWindow.disposeComposition()
+            modalWindow.dismiss()
+        }
+    }
+}
+
+/** Custom compose view for [BottomDrawer] */
+private class ModalWindow(
+    private var onDismissRequest: () -> Unit,
+    private val composeView: View,
+    saveId: UUID,
+) :
+    AbstractComposeView(composeView.context),
+    ViewTreeObserver.OnGlobalLayoutListener,
+    ViewRootForInspector {
+    init {
+        id = android.R.id.content
+        // Set up view owners
+        ViewTreeLifecycleOwner.set(this, ViewTreeLifecycleOwner.get(composeView))
+        ViewTreeViewModelStoreOwner.set(this, ViewTreeViewModelStoreOwner.get(composeView))
+        setViewTreeSavedStateRegistryOwner(composeView.findViewTreeSavedStateRegistryOwner())
+        setTag(androidx.compose.ui.R.id.compose_view_saveable_id_tag, "Popup:$saveId")
+        // Enable children to draw their shadow by not clipping them
+        clipChildren = false
+    }
+
+    private val windowManager =
+        composeView.context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+
+    private val displayWidth: Int
+        get() {
+            val density = context.resources.displayMetrics.density
+            return (context.resources.configuration.screenWidthDp * density).roundToInt()
+        }
+
+    private val params: WindowManager.LayoutParams =
+        WindowManager.LayoutParams().apply {
+            // Position bottom sheet from the bottom of the screen
+            gravity = Gravity.BOTTOM or Gravity.START
+            // Application panel window
+            type = WindowManager.LayoutParams.TYPE_APPLICATION_PANEL
+            // Fill up the entire app view
+            width = displayWidth
+            height = WindowManager.LayoutParams.MATCH_PARENT
+
+            // Format of screen pixels
+            format = PixelFormat.TRANSLUCENT
+            // Title used as fallback for a11y services
+            // TODO: Provide bottom sheet window resource
+            title = composeView.context.resources.getString(
+                androidx.compose.ui.R.string.default_popup_window_title
+            )
+            // Get the Window token from the parent view
+            token = composeView.applicationWindowToken
+
+            // Flags specific to modal bottom sheet.
+            flags = flags and (
+                    WindowManager.LayoutParams.FLAG_IGNORE_CHEEK_PRESSES or
+                            WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM
+                    ).inv()
+
+            flags = flags or WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        }
+
+    private var content: @Composable () -> Unit by mutableStateOf({})
+
+    override var shouldCreateCompositionOnAttachedToWindow: Boolean = false
+        private set
+
+    @Composable
+    override fun Content() {
+        content()
+    }
+
+    fun setCustomContent(
+        parent: CompositionContext? = null,
+        content: @Composable () -> Unit
+    ) {
+        parent?.let { setParentCompositionContext(it) }
+        this.content = content
+        shouldCreateCompositionOnAttachedToWindow = true
+    }
+
+    fun show() {
+        windowManager.addView(this, params)
+    }
+
+    fun dismiss() {
+        ViewTreeLifecycleOwner.set(this, null)
+        setViewTreeSavedStateRegistryOwner(null)
+        composeView.viewTreeObserver.removeOnGlobalLayoutListener(this)
+        windowManager.removeViewImmediate(this)
+    }
+
+    /**
+     * Taken from PopupWindow. Calls [onDismissRequest] when back button is pressed.
+     */
+    override fun dispatchKeyEvent(event: KeyEvent): Boolean {
+        if (event.keyCode == KeyEvent.KEYCODE_BACK) {
+            if (keyDispatcherState == null) {
+                return super.dispatchKeyEvent(event)
+            }
+            if (event.action == KeyEvent.ACTION_DOWN && event.repeatCount == 0) {
+                val state = keyDispatcherState
+                state?.startTracking(event, this)
+                return true
+            } else if (event.action == KeyEvent.ACTION_UP) {
+                val state = keyDispatcherState
+                if (state != null && state.isTracking(event) && !event.isCanceled) {
+                    onDismissRequest()
+                    return true
+                }
+            }
+        }
+        return super.dispatchKeyEvent(event)
+    }
+
+    override fun onGlobalLayout() {
+        // No-op
+    }
+
+    override fun setLayoutDirection(layoutDirection: Int) {
+        // Do nothing. ViewRootImpl will call this method attempting to set the layout direction
+        // from the context's locale, but we have one already from the parent composition.
+    }
+
+    // Sets the "real" layout direction for our content that we obtain from the parent composition.
+    fun superSetLayoutDirection(layoutDirection: LayoutDirection) {
+        val direction = when (layoutDirection) {
+            LayoutDirection.Ltr -> android.util.LayoutDirection.LTR
+            LayoutDirection.Rtl -> android.util.LayoutDirection.RTL
+        }
+        super.setLayoutDirection(direction)
+    }
+}
