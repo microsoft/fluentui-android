3a32b86cdafb2feb0c8b86d30778380b09dc0bd1
PraveenKumar yeruva <praveenyeruva970@gmail.com>
2023-10-04 13:57:05 +0530

    adding poeple picker vertical placement of items (#524)
    * adding poeple picker vertical placement of items
    
    * renaming pp token
    
    ---------
    
    Co-authored-by: PraveenKumar <pyeruva@microsoft.com>

--- fluentui_core/src/main/java/com/microsoft/fluentui/theme/token/controlTokens/PeoplePickerTokens.kt
+++ fluentui_core/src/main/java/com/microsoft/fluentui/theme/token/controlTokens/PeoplePickerTokens.kt
@@ -1,18 +1,8 @@
 package com.microsoft.fluentui.theme.token.controlTokens
 
-import android.os.Parcelable
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.SolidColor
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.Dp
-import com.microsoft.fluentui.theme.FluentTheme
-import com.microsoft.fluentui.theme.token.ControlInfo
-import com.microsoft.fluentui.theme.token.FluentAliasTokens
 import com.microsoft.fluentui.theme.token.FluentGlobalTokens
-import com.microsoft.fluentui.theme.token.IControlToken
 import kotlinx.parcelize.Parcelize
 
 open class PeoplePickerInfo() : TextFieldInfo()
@@ -21,7 +11,12 @@ open class PeoplePickerInfo() : TextFieldInfo()
 open class PeoplePickerTokens : TextFieldTokens() {
 
     @Composable
-    open fun chipSpacing(peoplePickerInfo: PeoplePickerInfo): Dp {
+    open fun chipHorizontalSpacing(peoplePickerInfo: PeoplePickerInfo): Dp {
         return FluentGlobalTokens.size(FluentGlobalTokens.SizeTokens.Size80)
+    }
+
+    @Composable
+    open fun chipVerticalSpacing(peoplePickerInfo: PeoplePickerInfo): Dp {
+        return FluentGlobalTokens.size(FluentGlobalTokens.SizeTokens.Size40)
     }
 }
