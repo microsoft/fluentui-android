8d3f896eab4ed783df14dddbc09a400198f33a8e
Anubhav Agrawal <68989156+Anubhvv@users.noreply.github.com>
2023-10-05 20:14:19 +0530

    Accessibility Role for Each BottomSheetDialog items (#523)
    * added accessibility delegate
    
    * added setAccessibilityDelegate in adapter
    
    * SetAccessibilityDelegate rightly configured and set
    
    * Removing Unused imports
    
    * Default roleDescription is an empty string, and added an example of roleDescription from activity
    
    * changed roleDescription from clickable button to button
    
    ---------
    
    Co-authored-by: Anubhav Agrawal <anubagrawal@microsoft.com>

--- fluentui_drawer/src/main/java/com/microsoft/fluentui/bottomsheet/BottomSheetItem.kt
+++ fluentui_drawer/src/main/java/com/microsoft/fluentui/bottomsheet/BottomSheetItem.kt
@@ -43,6 +43,8 @@ class BottomSheetItem : Parcelable {
     val accessoryImageId: Int
     val accessoryBitmap: Bitmap?
 
+    val roleDescription: String
+
     @JvmOverloads
     constructor(
         id: Int = NO_ID,
@@ -55,7 +57,8 @@ class BottomSheetItem : Parcelable {
         customBitmap: Bitmap? = null,
         disabled: Boolean = false,
         @DrawableRes accessoryImageId: Int = NO_ID,
-        accessoryBitmap: Bitmap? = null
+        accessoryBitmap: Bitmap? = null,
+        roleDescription: String = "",
     ) {
         this.id = id
         this.imageId = imageId
@@ -68,6 +71,7 @@ class BottomSheetItem : Parcelable {
         this.disabled = disabled
         this.accessoryImageId = accessoryImageId
         this.accessoryBitmap = accessoryBitmap
+        this.roleDescription = roleDescription
     }
 
     private constructor(parcel: Parcel) : this(
@@ -81,7 +85,8 @@ class BottomSheetItem : Parcelable {
         customBitmap = parcel.readParcelable(Bitmap::class.java.classLoader),
         disabled = parcel.readInt() == 1,
         accessoryImageId = parcel.readInt(),
-        accessoryBitmap = parcel.readParcelable(Bitmap::class.java.classLoader)
+        accessoryBitmap = parcel.readParcelable(Bitmap::class.java.classLoader),
+        roleDescription = parcel.readString() ?: "",
     )
 
     override fun writeToParcel(parcel: Parcel, flags: Int) {
@@ -96,6 +101,7 @@ class BottomSheetItem : Parcelable {
         parcel.writeInt(if (disabled) 1 else 0)
         parcel.writeInt(accessoryImageId)
         parcel.writeValue(accessoryBitmap)
+        parcel.writeString(roleDescription)
     }
 
     override fun describeContents(): Int = 0
@@ -116,6 +122,7 @@ class BottomSheetItem : Parcelable {
         if (disabled != other.disabled) return false
         if (accessoryImageId != other.accessoryImageId) return false
         if (accessoryBitmap != other.accessoryBitmap) return false
+        if (roleDescription != other.roleDescription) return false
 
         return true
     }
@@ -132,6 +139,7 @@ class BottomSheetItem : Parcelable {
         result = 31 * result + disabled.hashCode()
         result = 31 * result + accessoryImageId.hashCode()
         result = 31 * result + (accessoryBitmap?.hashCode() ?: 0)
+        result = 31 * result + roleDescription.hashCode()
         return result
     }
 
