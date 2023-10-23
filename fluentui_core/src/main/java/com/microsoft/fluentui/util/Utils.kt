086935675b6c252016285e2a67b424ab71371a98
PraveenKumar <pyeruva@microsoft.com>
2023-10-23 11:17:14 +0530

    adding string resourvce functon

--- fluentui_core/src/main/java/com/microsoft/fluentui/util/Utils.kt
+++ fluentui_core/src/main/java/com/microsoft/fluentui/util/Utils.kt
@@ -18,3 +18,7 @@ fun dpToPx(value: Dp) = (value * Resources
 fun getStringResource(id: Int): String {
     return LocalContext.current.resources.getString(id)
 }
+@Composable
+fun getStringResource(id: Int, vararg formatArgs: Any): String {
+    return LocalContext.current.resources.getString(id, *formatArgs)
+}
