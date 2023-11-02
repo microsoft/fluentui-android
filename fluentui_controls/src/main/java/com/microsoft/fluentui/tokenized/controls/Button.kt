16199a5c572c1cf3c1be25c7205662c30e4ff44f
Anubhav Agrawal <anubagrawal@microsoft.com>
2023-11-02 14:37:20 +0530

    removing this extra file from commit attempt 2

--- fluentui_controls/src/main/java/com/microsoft/fluentui/tokenized/controls/Button.kt
+++ fluentui_controls/src/main/java/com/microsoft/fluentui/tokenized/controls/Button.kt
@@ -17,8 +17,8 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.semantics.*
 import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.ExperimentalTextApi
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.microsoft.fluentui.theme.FluentTheme
