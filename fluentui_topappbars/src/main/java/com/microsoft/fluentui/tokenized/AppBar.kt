fb6808637a6f1453716fd0aa06491f235b14e73a
Anubhav Agrawal <68989156+Anubhvv@users.noreply.github.com>
2023-12-07 21:52:42 +0530

    Ripple Effect for navigation Icon in v2 App Bar (#568)
    * adding madifier param in AppBar
    
    * default ripple behavior
    
    * added param description
    
    * removing modifier param
    
    * certificate/cla not received
    
    * last change reverting
    
    ---------
    
    Co-authored-by: Anubhav Agrawal <anubagrawal@microsoft.com>

--- fluentui_topappbars/src/main/java/com/microsoft/fluentui/tokenized/AppBar.kt
+++ fluentui_topappbars/src/main/java/com/microsoft/fluentui/tokenized/AppBar.kt
@@ -3,9 +3,12 @@ package com.microsoft.fluentui.tokenized
 import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.BasicText
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
@@ -140,7 +143,14 @@ fun AppBar(
                 if (appBarSize != AppBarSize.Large && navigationIcon.isIconAvailable()) {
                     Icon(
                         navigationIcon,
-                        modifier = Modifier
+                        modifier =
+                        Modifier
+                            .clickable(
+                                interactionSource = remember { MutableInteractionSource() },
+                                indication = rememberRipple(),
+                                enabled = true,
+                                onClick = navigationIcon.onClick ?: {}
+                            )
                             .padding(token.navigationIconPadding(appBarInfo))
                             .size(token.leftIconSize(appBarInfo)),
                         tint = token.navigationIconColor(appBarInfo)
