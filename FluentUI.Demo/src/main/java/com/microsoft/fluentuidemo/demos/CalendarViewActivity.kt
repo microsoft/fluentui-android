cd8ba456419991eb5ed9e694f8daa1c7f036952e
Anubhav Agrawal <68989156+Anubhvv@users.noreply.github.com>
2024-04-02 14:33:57 +0530

    V1 Calendar Accessibility: Added a way to leave focus when using Hardware Keyboard (#633)
    * leave focus when tab pressed
    
    * removing calendarView.kt changes
    
    * removing unused import
    
    ---------
    
    Co-authored-by: Anubhav Agrawal <anubagrawal@microsoft.com>

--- FluentUI.Demo/src/main/java/com/microsoft/fluentuidemo/demos/CalendarViewActivity.kt
+++ FluentUI.Demo/src/main/java/com/microsoft/fluentuidemo/demos/CalendarViewActivity.kt
@@ -6,7 +6,9 @@
 package com.microsoft.fluentuidemo.demos
 
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.LayoutInflater
+import android.view.View
 import android.view.View.TEXT_ALIGNMENT_TEXT_START
 import com.microsoft.fluentui.calendar.OnDateSelectedListener
 import com.microsoft.fluentui.util.DateStringUtils
@@ -63,4 +65,22 @@ class CalendarViewActivity : DemoActivity() {
         calenderBinding.exampleDateTitle.text = DateStringUtils.formatDateWithWeekDay(this, date)
         calenderBinding.calendarView.setSelectedDateRange(date.toLocalDate(), Duration.ZERO, false)
     }
+
+    /**
+     * This function allows the user to leave the calendar focus by pressing the tab key.
+     * @param  keyCode The value in event.getKeyCode().
+     * @param  event The KeyEvent object.
+     */
+    override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_TAB  && calenderBinding.calendarView.hasFocus()) {
+            // Find the currently focused view
+            val focusedView = currentFocus
+            // Remove focus from the currently focused view
+            focusedView?.clearFocus()
+
+            return true // Consume the event
+        }
+        return super.onKeyDown(keyCode, event)
+    }
+
 }
