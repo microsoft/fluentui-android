d4513b4e8f7c5c4b9b639642512b843f549b4f95
PraveenKumar yeruva <praveenyeruva970@gmail.com>
2024-07-02 11:36:36 +0530

    centering the bottomsheet on custom width (#668)
    Co-authored-by: PraveenKumar <pyeruva@microsoft.com>

--- fluentui_drawer/src/main/java/com/microsoft/fluentui/tokenized/bottomsheet/BottomSheet.kt
+++ fluentui_drawer/src/main/java/com/microsoft/fluentui/tokenized/bottomsheet/BottomSheet.kt
@@ -312,6 +312,7 @@ fun BottomSheet(
 
         Box(
             Modifier
+                .align(Alignment.Center)
                 .fillMaxWidth(
                     if(configuration.orientation == Configuration.ORIENTATION_LANDSCAPE)maxLandscapeWidth
                     else 1F
